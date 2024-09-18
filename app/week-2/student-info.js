0e8613ba822ab880a1d9370928f44e739b8392c9
Harshdeep Kaur <152572971+harshdeepkaur1311@users.noreply.github.com>
2024-09-18 13:07:52 -0600

    Week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";   
+export default function StudentInfo() {
+    return(
+       <div>
+        <p>Harshdeep Kaur</p>
+        
+            <Link href="https://github.com">https://github.com</Link>
+        
+       </div>
+    
+    );
+}
